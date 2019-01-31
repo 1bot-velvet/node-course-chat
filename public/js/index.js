607d5eaa9b89b3106b429343a742d59c19b1f694
Only Person <1bot@velvetray.com>
2019-01-31 05:51:18 +0000

    Emit messages

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'Mike',
-    text: 'why not?'
-  });
+  // socket.emit('createMessage', {
+  //   from: 'Mike',
+  //   text: 'why not?'
+  // });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Bob_1001',
-    text: 'Not tonight mate',
-    createdAt: 5001
-  });
+  // socket.emit('newMessage', {
+  //   from: 'Bob_1001',
+  //   text: 'Not tonight mate',
+  //   createdAt: 5001
+  // });
 
   socket.on('createMessage', (message) => {
-    console.log('createMessage', message);
+//    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   // socket.on('createEmail', (newEmail) => {
