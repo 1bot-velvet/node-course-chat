0628e9afac6b61a09920994be3e0fef7641121a1
Only Person <1bot@velvetray.com>
2019-02-07 08:45:34 +0000

    add goelocation support

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,7 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
+
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -12,3 +13,20 @@ describe('generateMessage', () => {
     expect(message).toInclude({from, text})
   });
 });
+
+
+describe('generateLocationMessage', () => {
+  it('Should generate correct location object', () => {
+    var from = 'bob101';
+    var latitude = '51.1111';
+    var longitude = '-1.56665';
+    var url = 'https://www.google.com/maps?q=51.1111,-1.56665';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
+  });
+});
+
+
+// from lat & longitude
